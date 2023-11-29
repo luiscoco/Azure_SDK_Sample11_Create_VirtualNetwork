184346ec7c8326c7d9201e4f7efcff1e9a65f944
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-11-29 17:27:52 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Network;
+using Azure.ResourceManager.Network.Models;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Resources.Models;
+
+ArmClient armClient = new ArmClient(new DefaultAzureCredential());
+SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+
+ResourceGroupCollection rgCollection = subscription.GetResourceGroups();
+// With the collection, we can create a new resource group with an specific name
+string rgName = "myRgName";
+AzureLocation location = AzureLocation.WestUS2;
+ResourceGroupResource resourceGroup = await rgCollection.CreateOrUpdate(WaitUntil.Started, rgName, new ResourceGroupData(location)).WaitForCompletionAsync();
+
+VirtualNetworkCollection virtualNetworkCollection = resourceGroup.GetVirtualNetworks();
+
+string vnetName = "myVnet";
+
+// Use the same location as the resource group
+VirtualNetworkData input = new VirtualNetworkData()
+{
+    Location = resourceGroup.Data.Location,
+    AddressPrefixes = { "10.0.0.0/16", },
+    DhcpOptionsDnsServers = { "10.1.1.1", "10.1.2.4" },
+    Subnets = { new SubnetData() { Name = "mySubnet", AddressPrefix = "10.0.1.0/24", } }
+};
+
+VirtualNetworkResource vnet = await virtualNetworkCollection.CreateOrUpdate(WaitUntil.Completed, vnetName, input).WaitForCompletionAsync();
+
+VirtualNetworkCollection virtualNetworkCollection1 = resourceGroup.GetVirtualNetworks();
+
+VirtualNetworkResource virtualNetwork1 = await virtualNetworkCollection1.GetAsync("myVnet");
+Console.WriteLine(virtualNetwork1.Data.Name);
+
+
